54595d6f51ffb8d97a5f71e16e46fdf6e5c09c20
colinrdavidson <colinrdavidson@gmail.com>
2015-12-05 20:16:00 -0800

    Created .gitignore Added in another job that depends on our other job

--- hello_world.py
+++ hello_world.py
@@ -3,7 +3,6 @@
 
 import luigi
 
-    
 class SimpleTask(luigi.Task):
     """
     This simple task prints Hello World!
@@ -17,9 +16,28 @@ class SimpleTask(luigi.Task):
         _out.write(u"Hello World!\n")
         _out.close()
  
+class OtherTask(luigi.Task):
+  """
+  Other task to play with
+  """
+
+  def output(self):
+    return luigi.LocalTarget("output2.txt")
+
+  def requires(self):
+    return SimpleTask()
+
+  def run(self):
+    _in = self.input().open("r")
+    _out = self.output().open('w')
+    for line in _in:
+        outval = u"And Again: "+line+u"\n"
+    
+    _out.close()
+    _in.close()
 
 if __name__ == '__main__':
     from luigi.mock import MockFile # import this here for compatibility with Windows
     # if you are running windows, you wouldn need --lock-pid-dir argument; modified run would look like
     # luigi.run(["--lock-pid-dir", "D:\\temp\\", "--local-scheduler"], main_task_cls=SimpleTask)
-    luigi.run(["--local-scheduler"], main_task_cls=SimpleTask)
+    luigi.run(["--local-scheduler"], main_task_cls=OtherTask)
